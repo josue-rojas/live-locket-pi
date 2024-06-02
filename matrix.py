b745b68c9f7db5866555308b2b833ade6af37e0e
josue <josuerojas.rojas@gmail.com>
2024-06-02 11:33:05 -0400

    [TEST] Test script

--- matrix.py
+++ matrix.py
@@ -6,8 +6,8 @@ import sys
 from io import BytesIO
 import sys,os
 
-# from rgbmatrix import RGBMatrix, RGBMatrixOptions
-# from PIL import Image
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
+from PIL import Image
 from datetime import datetime
 import configparser
 
@@ -42,25 +42,25 @@ image_file = os.path.join(dir, IMAGES_DIR, './example.png')
 print(os.path.join(dir, IMAGES_DIR, './example.png'))
 print(getStartImage()[3])
 
-# options = RGBMatrixOptions()
-# options.hardware_mapping = 'adafruit-hat' 
-# options.rows = int(config['DEFAULT']['rows'])
-# options.cols = int(config['DEFAULT']['columns'])
-# options.chain_length = int(config['DEFAULT']['chain_length'])
-# options.parallel = int(config['DEFAULT']['parallel'])
-# options.hardware_mapping = config['DEFAULT']['hardware_mapping']
-# options.gpio_slowdown = int(config['DEFAULT']['gpio_slowdown'])
-# options.brightness = int(config['DEFAULT']['brightness'])
-# options.limit_refresh_rate_hz = int(config['DEFAULT']['refresh_rate'])
+options = RGBMatrixOptions()
+options.hardware_mapping = 'adafruit-hat' 
+options.rows = int(config['DEFAULT']['rows'])
+options.cols = int(config['DEFAULT']['columns'])
+options.chain_length = int(config['DEFAULT']['chain_length'])
+options.parallel = int(config['DEFAULT']['parallel'])
+options.hardware_mapping = config['DEFAULT']['hardware_mapping']
+options.gpio_slowdown = int(config['DEFAULT']['gpio_slowdown'])
+options.brightness = int(config['DEFAULT']['brightness'])
+options.limit_refresh_rate_hz = int(config['DEFAULT']['refresh_rate'])
 
 
-# # image = Image.open(image_file)
+image = Image.open(image_file)
 
-# # matrix = RGBMatrix(options=options)
+matrix = RGBMatrix(options=options)
 
 # # # Create a thumbnail that first our screen
-# # image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
-# # matrix.SetImage(image.convert('RGB'))
+image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
+matrix.SetImage(image.convert('RGB'))
 
 # # try:
 # #     print("Press CTRL-C to stop.")
