132dc4475cac5be6856f4332ea0ad4bc31654ce0
josue <josuerojas.rojas@gmail.com>
2024-06-04 12:38:30 -0400

    [FEAT] Update matrix.py

---
+++ constants.py
@@ -0,0 +1,11 @@
+IMAGES_DIR = "./images"
+CONFIG_SRC = './config/rgb_options.ini'
+IMAGES_DB = 'images.db'
+
+BASE_IMAGES_API = 'https://rphgcdgdqylokpeygnyy.supabase.co/functions/v1/'
+GET_ALL_IMAGES_API = BASE_IMAGES_API + 'image-all'
+DELETE_IMAGE_BY_ID = BASE_IMAGES_API + 'image-delete'
+
+IMAGES_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwaGdjZGdkcXlsb2twZXlnbnl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY0ODY3NTgsImV4cCI6MjAzMjA2Mjc1OH0.l5Cmj0CM3B3vc5BxAHOxMtx4puIKmZHjdEH9Hh7eKoc'
+
+IMAGES_BUCKET_BASE = 'https://rphgcdgdqylokpeygnyy.supabase.co/storage/v1/object/public/images'
