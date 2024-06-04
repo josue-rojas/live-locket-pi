9beef7f7367326dcc8190aab6035afc8e8309a55
josue <josuerojas.rojas@gmail.com>
2024-06-04 13:42:13 -0400

    [FEAT] Images service created

--- init.sh
+++ init.sh
@@ -38,3 +38,29 @@ sudo systemctl daemon-reload
 sudo systemctl start matrix
 sudo systemctl enable matrix
 echo "...done"
+
+
+# --- Images service setup
+echo "Removing images service if it exists:"
+sudo systemctl stop images
+sudo systemctl disable images
+sudo rm -rf /etc/systemd/system/images.*
+sudo systemctl daemon-reload
+echo "...done"
+
+echo "Creating  images service:"
+# Ensure the script is executable
+sudo chmod +x "${install_path}/images.py"
+sudo cp ./config/images.service /etc/systemd/system/
+sudo sed -i -e "/\[Service\]/a ExecStart=${install_path}/images.py" /etc/systemd/system/images.service
+sudo sed -i -e "/\[Service\]/a WorkingDirectory=${install_path}/" /etc/systemd/system/images.service
+
+# Create service.d directory if it doesn't exist
+sudo mkdir -p /etc/systemd/system/images.service.d
+images_env_path=/etc/systemd/system/images.service.d/images_env.conf
+sudo touch $images_env_path
+echo "[Service]" | sudo tee $images_env_path
+sudo systemctl daemon-reload
+sudo systemctl start images
+sudo systemctl enable images
+echo "...done"
