c3b2b85f6b02bf6ece3abbb14bfa6171291ee5ab
josue <josuerojas.rojas@gmail.com>
2024-06-18 11:23:47 -0400

    [FEAT] Revert matrix script

--- matrix.py
+++ matrix.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 import time
 import sys
-import os
+import sys,os
+
 from rgbmatrix import RGBMatrix, RGBMatrixOptions
 from PIL import Image
+from datetime import datetime
 import configparser
 from imageRepository import get_random_image
 from constants import IMAGES_DIR, CONFIG_SRC
@@ -12,58 +14,58 @@ def getNextRandomFile(currentFile):
     nextFile = currentFile
     while currentFile == nextFile:
         nextFile = get_random_image()[3]
+
     return nextFile
 
-def load_config():
-    dir = os.path.dirname(__file__)
-    configFileName = os.path.join(dir, CONFIG_SRC)
-    config = configparser.ConfigParser()
-    config.read(configFileName)
-    return config
+dir = os.path.dirname(__file__)
 
-def configure_matrix(config):
-    options = RGBMatrixOptions()
-    options.hardware_mapping = 'adafruit-hat' 
-    options.rows = int(config['DEFAULT']['rows'])
-    options.cols = int(config['DEFAULT']['columns'])
-    options.chain_length = int(config['DEFAULT']['chain_length'])
-    options.parallel = int(config['DEFAULT']['parallel'])
-    options.hardware_mapping = config['DEFAULT']['hardware_mapping']
-    options.gpio_slowdown = int(config['DEFAULT']['gpio_slowdown'])
-    options.brightness = int(config['DEFAULT']['brightness'])
-    options.limit_refresh_rate_hz = int(config['DEFAULT']['refresh_rate'])
-    return RGBMatrix(options=options)
+configFileName = os.path.join(dir, CONFIG_SRC)
+config = configparser.ConfigParser()
+config.read(configFileName)
+startImage = get_random_image()[3]
+image_file = os.path.join(dir, IMAGES_DIR, startImage)
 
-def display_image(matrix, image_path):
-    image = Image.open(image_path)
-    image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
-    matrix.SetImage(image.convert('RGB'))
+options = RGBMatrixOptions()
+options.hardware_mapping = 'adafruit-hat' 
+options.rows = int(config['DEFAULT']['rows'])
+options.cols = int(config['DEFAULT']['columns'])
+options.chain_length = int(config['DEFAULT']['chain_length'])
+options.parallel = int(config['DEFAULT']['parallel'])
+options.hardware_mapping = config['DEFAULT']['hardware_mapping']
+options.gpio_slowdown = int(config['DEFAULT']['gpio_slowdown'])
+options.brightness = int(config['DEFAULT']['brightness'])
+options.limit_refresh_rate_hz = int(config['DEFAULT']['refresh_rate'])
 
-def main():
-    config = load_config()
-    image_timer = int(config['DEFAULT']['image_timer'])
-    matrix = configure_matrix(config)
-    dir = os.path.dirname(__file__)
-    print(dir)
+image_timer = int(config['DEFAULT']['image_timer'])
+image = Image.open(image_file)
 
-    startImage = get_random_image()[3]
+matrix = RGBMatrix(options=options)
+
+# Create a thumbnail that fits our screen
+image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
+matrix.SetImage(image.convert('RGB'))
+
+try:
+    print("Press CTRL-C to stop.")
     currentFile = startImage
-    currentImageFilePath = os.path.join(dir, IMAGES_DIR, currentFile)
 
-    display_image(matrix, currentImageFilePath)
+    while True:
+        try:
+            now = datetime.now()
+            current_time = now.strftime("%H:%M:%S")
+            print("Current Time =", current_time)
 
-    try:
-        print("Press CTRL-C to stop.")
-        while True:
-            try:
-                currentFile = getNextRandomFile(currentFile)
-                currentImageFilePath = os.path.join(dir, IMAGES_DIR, currentFile)
-                display_image(matrix, currentImageFilePath)
-            except Exception as e:
-                print("Error:", e)
-                display_image(matrix, currentImageFilePath)
-            time.sleep(image_timer)
-    except KeyboardInterrupt:
-        sys.exit(0)
+            currentFile = getNextRandomFile(currentFile)
+            currentImageFilePath = os.path.join(dir, IMAGES_DIR, currentFile)
+            nextImage = Image.open(currentImageFilePath)
 
-main()
+            nextImage.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
+            matrix.SetImage(nextImage.convert('RGB'))
+            print('setting image', currentFile)
+        except Exception as e:
+            print("Error----")
+            print(e)
+            matrix.SetImage(image.convert('RGB'))
+        time.sleep(image_timer)
+except KeyboardInterrupt:
+    sys.exit(0)
